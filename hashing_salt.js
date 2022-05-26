849638061c347048ed9ae5d3a101be5f60ab5872
Jatin Dhingra <68529999+jatin-dhingra@users.noreply.github.com>
2022-05-26 21:09:53 +0530

    Create hashing_salt.js

---
+++ hashing_salt.js
@@ -0,0 +1,10 @@
+const bcrypt= require('bcrypt');
+
+const hashPassword= async(pw)=>{
+    const salt= await bcrypt.genSalt(10);
+    const hash= await bcrypt.hash(pw,salt);
+    console.log(salt);
+    console.log(hash);
+}
+
+hashPassword('xnxx');
